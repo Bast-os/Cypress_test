9d50f3f4c90fc26f68550fc08ad02e51b24d5109
Bast-os <bastien.pelletier@ginkoia.fr>
2024-02-26 16:43:46 +0100

    tutorial

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,15 @@ describe('Training', () => {
     cy.visit('https://example.cypress.io')
 
     cy.contains('type').click()
+
+    //verify url
+    cy.url().should('include','/commands/actions')
+
+    //get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+
+    //verify value has been updated
+    cy.get('.action-email').should('have.value','fake@email.com')
   })
 })
 
